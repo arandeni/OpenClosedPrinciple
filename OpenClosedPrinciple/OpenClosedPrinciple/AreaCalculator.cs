a579ac3629a58f3a634f4fbc000f85f238f33141
Achini Randeni <achinirandeni@gmail.com>
2024-06-26 18:32:16 +0800

    Initial commit

---
+++ OpenClosedPrinciple/OpenClosedPrinciple/AreaCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public class AreaCalculator
+    {
+        //but everytime we introduce a new shape, we must alter the TotalArea method.
+        //so the AreaCalculator class is not closed for modifications
+        public double TotalArea(object[] arrObjects) 
+        {
+            double area = 0;
+            Rectangle objRectangle;
+            Circle objCircle;
+            foreach (var obj in arrObjects) 
+            {
+                if (obj is Rectangle)
+                {
+                    objRectangle = (Rectangle)obj;
+                    area += objRectangle.Height * objRectangle.Width;
+                }
+                else 
+                {
+                    objCircle = (Circle)obj;
+                    area += objCircle.Radius * objCircle.Radius * Math.PI;
+                }
+            }
+            return area;
+        }
+    }
+}
---
+++ OpenClosedPrinciple/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using OpenClosedPrinciple;
+
+AreaCalculator cal = new AreaCalculator();
+object[] arr = new object[2];
+Rectangle rec = new Rectangle() 
+{ 
+    Height = 2, Width = 3
+};
+Circle cr = new Circle()
+{
+    Radius = 2
+};
+arr[0] = rec;
+arr[1] = cr;
+double area = cal.TotalArea(arr);
+Console.WriteLine(area);
+
+AreaCalculatorNew cal2 = new AreaCalculatorNew();
+Shape[] shapes = new Shape[2];
+RectangleNew recNew = new RectangleNew() 
+{
+    Height = 2,
+    Width = 3
+};
+CircleNew cr2 = new CircleNew()
+{
+    Radius = 2
+};
+shapes[0] = recNew;
+shapes[1] = cr2;
+double area2 = cal2.TotalArea(shapes);
+Console.WriteLine(area2);
+
